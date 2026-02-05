8debb5fdbaa2ee7d2e24ca583ad74cc7ee2d5a79
Leo <leozinho17silvacosta@gmail.com>
2026-02-04 20:56:36 -0800

    Complete TypeScript Core Exercise OOP and TS

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,13 +1,13 @@
+"use strict";
 // Exercise: Create a function `setTheme` that accepts a parameter with the literal type 'dark' or 'light'.
 // The function should return a string saying "Theme set to [theme]".
-
-function setTheme(theme: "dark" | "light") {
-    if(theme === "dark") {
-        return `Theme set to dark`
-    } else {
-        return `Theme set to light`
+function setTheme(theme) {
+    if (theme === "dark") {
+        return `Theme set to dark`;
+    }
+    else {
+        return `Theme set to light`;
     }
 }
-
 console.log(setTheme('dark'));
 // Expected output: "Theme set to dark"
